916acb5e172ff9d5dff896c11398524eb7827c0a
Prajwal Raut <rautprajwal546@gmail.com>
2023-03-07 00:30:33 +0530

    added file to the env
    added file to the env

--- dbConnect.js
+++ dbConnect.js
@@ -1,9 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+require('dotenv').config();
+const dbURL = process.env.dbURL;
 
-mongoose.connect('mongodb+srv://prajwal:prajwal0403@banking.3hga9.mongodb.net/Money' , {useNewUrlParser : true , useUnifiedTopology : true})
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-const connection = mongoose.connection
+const connection = mongoose.connection;
 
-connection.on('error', err => console.log(err))
+connection.on('error', (err) => console.log(err));
 
-connection.on('connected' , () => console.log('Mongo DB Connection Successfull'))
+connection.on('connected', () =>
+  console.log('Mongo DB Connection Successfull')
+);
